242e2bd30dc1d2833a649bc976b4712a592ffe9c
Frederik Hahne <atomfrede@gmail.com>
2019-08-04 07:07:40 +0200

    :loud_sound: exceptions are logged as before

--- src/main/java/com/mycompany/myapp/service/InvalidPasswordException.java
+++ src/main/java/com/mycompany/myapp/service/InvalidPasswordException.java
@@ -1,4 +1,8 @@
 package com.mycompany.myapp.service;
 
 public class InvalidPasswordException extends RuntimeException {
+
+    public InvalidPasswordException() {
+        super("Incorrect password");
+    }
 }
--- src/main/java/com/mycompany/myapp/service/LoginAlreadyUsedException.java
+++ src/main/java/com/mycompany/myapp/service/LoginAlreadyUsedException.java
@@ -1,4 +1,8 @@
 package com.mycompany.myapp.service;
 
 public class LoginAlreadyUsedException extends RuntimeException {
+
+    public LoginAlreadyUsedException() {
+        super("Login name already used!");
+    }
 }
