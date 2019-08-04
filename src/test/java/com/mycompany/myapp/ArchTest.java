0220f0c5de935d0bfa78d4e19a5db366642ed30c
Frederik Hahne <atomfrede@gmail.com>
2019-08-04 06:54:00 +0200

    :white_check_mark: test that service layer does not depend on web layer

--- src/test/java/com/mycompany/myapp/ArchTest.java
+++ src/test/java/com/mycompany/myapp/ArchTest.java
@@ -1,4 +1,28 @@
 package com.mycompany.myapp;
 
-public class ArchTest {
+import com.tngtech.archunit.core.domain.JavaClasses;
+import com.tngtech.archunit.core.importer.ClassFileImporter;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.noClasses;
+
+class ArchTest {
+
+    @Test
+    @DisplayName("Services and Repositories should not depend on web layer")
+    void services_and_repositories_should_not_depend_on_web_layer() {
+
+        JavaClasses importedClasses = new ClassFileImporter().importPackages("com.mycompany.myapp");
+
+        noClasses()
+            .that()
+                .resideInAnyPackage("..service..")
+            .or()
+                .resideInAnyPackage("..repository..")
+            .should().dependOnClassesThat()
+                .resideInAnyPackage("..com.mycompany.myapp.web..")
+        .because("Services and repositories should not depend on web layer")
+        .check(importedClasses);
+    }
 }
